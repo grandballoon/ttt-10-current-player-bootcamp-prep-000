167976cfcf228f4dc46c2430e9e4580c132f06dd
Luke Conley <luke.sovern.conley@gmail.com>
2018-04-19 19:44:19 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def turn_count(board)
+turn_number = 0
+board.each do |counter|
+  if counter == "X" || counter == "O"
+    turn_number += 1
+  end
+end
+turn_number
+end
+
+def current_player(board)
+if turn_count % 2 = 0
+  "X"
+else 
+  "O"
+end
+end
